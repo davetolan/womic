166bdfad892d1ec132fdf09ed29f922c61059797
Dave Tolan <tolan.david@gmail.com>
2026-02-18 21:39:41 -0600

    Add fresh build

--- next-comic-platform/src/migrations/index.ts
+++ next-comic-platform/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260219_033523 from './20260219_033523';
+import * as migration_20260219_033706 from './20260219_033706';
 
 export const migrations = [
   {
     up: migration_20260219_033523.up,
     down: migration_20260219_033523.down,
-    name: '20260219_033523'
+    name: '20260219_033523',
+  },
+  {
+    up: migration_20260219_033706.up,
+    down: migration_20260219_033706.down,
+    name: '20260219_033706'
   },
 ];
