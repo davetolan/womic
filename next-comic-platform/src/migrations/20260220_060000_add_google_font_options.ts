43543983e7900b714716124948441271bfafc767
davetolan <tolan.david@gmail.com>
2026-02-20 20:04:59 -0600

    Fix font enum migration for parameterized DO block failure

--- next-comic-platform/src/migrations/20260220_060000_add_google_font_options.ts
+++ next-comic-platform/src/migrations/20260220_060000_add_google_font_options.ts
@@ -1,55 +1,46 @@
 import { MigrateDownArgs, MigrateUpArgs, sql } from '@payloadcms/db-postgres'
 
-const addEnumValueIfMissing = (enumName: string, value: string) => sql`
-  DO $$
-  BEGIN
-    IF EXISTS (
-      SELECT 1
-      FROM pg_type t
-      JOIN pg_namespace n ON n.oid = t.typnamespace
-      WHERE n.nspname = 'public'
-        AND t.typname = ${enumName}
-    ) AND NOT EXISTS (
-      SELECT 1
-      FROM pg_type t
-      JOIN pg_enum e ON e.enumtypid = t.oid
-      JOIN pg_namespace n ON n.oid = t.typnamespace
-      WHERE n.nspname = 'public'
-        AND t.typname = ${enumName}
-        AND e.enumlabel = ${value}
-    ) THEN
-      EXECUTE format('ALTER TYPE %I.%I ADD VALUE %L', 'public', ${enumName}, ${value});
-    END IF;
-  END $$;
-`
-
 export async function up({ db }: MigrateUpArgs): Promise<void> {
-  const fonts = [
-    'roboto',
-    'openSans',
-    'lato',
-    'montserrat',
-    'oswald',
-    'sourceSans3',
-    'raleway',
-    'poppins',
-    'merriweather',
-    'nunito',
-  ]
-
-  const enumNames = [
-    'enum_site_settings_default_font',
-    'enum_pages_font_override',
-    'enum__pages_v_version_font_override',
-    'enum_posts_font_override',
-    'enum__posts_v_version_font_override',
-  ]
-
-  for (const enumName of enumNames) {
-    for (const font of fonts) {
-      await db.execute(addEnumValueIfMissing(enumName, font))
-    }
-  }
+  await db.execute(sql`
+    DO $$
+    DECLARE
+      enum_name text;
+      font_value text;
+      enum_names text[] := ARRAY[
+        'enum_site_settings_default_font',
+        'enum_pages_font_override',
+        'enum__pages_v_version_font_override',
+        'enum_posts_font_override',
+        'enum__posts_v_version_font_override'
+      ];
+      font_values text[] := ARRAY[
+        'roboto',
+        'openSans',
+        'lato',
+        'montserrat',
+        'oswald',
+        'sourceSans3',
+        'raleway',
+        'poppins',
+        'merriweather',
+        'nunito'
+      ];
+    BEGIN
+      FOREACH enum_name IN ARRAY enum_names LOOP
+        IF EXISTS (
+          SELECT 1
+          FROM pg_type t
+          JOIN pg_namespace n ON n.oid = t.typnamespace
+          WHERE n.nspname = 'public'
+            AND t.typname = enum_name
+        ) THEN
+          FOREACH font_value IN ARRAY font_values LOOP
+            EXECUTE format('ALTER TYPE %I.%I ADD VALUE IF NOT EXISTS %L', 'public', enum_name, font_value);
+          END LOOP;
+        END IF;
+      END LOOP;
+    END $$;
+  `)
 }
 
 export async function down({ db }: MigrateDownArgs): Promise<void> {
